8dd9cab3950803890da4bab61d9a1c40db64b810
sibainu2 <azusa.gomibako@gmail.com>
2023-08-15 08:00:05 +0900

    Flake8の対応

--- anti-apam/setting/anti_spam_tool/dataclass.py
+++ anti-apam/setting/anti_spam_tool/dataclass.py
@@ -1,25 +1,29 @@
 from sqlalchemy import DateTime
 import datetime
+
+
 class User():
-    def __init__(self,id,name,mute,message_count,threat_level) -> None:
+    def __init__(self, id, name, mute, message_count, threat_level) -> None:
         self.id = id
         self.name = name
         self.mute = mute
         self.message_count = message_count
         self.threat_level = threat_level
 
+
 class Message():
-    def __init__(self,id:int,user:User,content:str,threat_level) -> None:
+    def __init__(self, id: int, user: User, content: str, threat_level) -> None:
         self.id = id
         self.user = user
         self.content = content
         self.threat_level = threat_level
 
+
 class SpamSession():
     def __init__(self) -> None:
-        send_all_user = []
-        mute_user = []
-        all_message = []
-        start_time:DateTime = datetime.datetime.utcnow()
-        latest_time:DateTime = datetime.datetime.utcnow()
-        end_time:DateTime = None
+        self.send_all_user = []
+        self.mute_user = []
+        self.all_message = []
+        self.start_time: DateTime = datetime.datetime.utcnow()
+        self.latest_time: DateTime = datetime.datetime.utcnow()
+        self.end_time: DateTime = None
