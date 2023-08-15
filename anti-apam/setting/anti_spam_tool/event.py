078d44b749677364bacadab6579791cc006ead9b
sibainu2 <azusa.gomibako@gmail.com>
2023-08-15 11:29:11 +0900

    flake8の対応

--- anti-apam/setting/anti_spam_tool/event.py
+++ anti-apam/setting/anti_spam_tool/event.py
@@ -1,25 +1,28 @@
-import sys
-sys.path.append('../')
-import time
-import asyncio
-import datetime
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime, Boolean
+from sqlalchemy import (
+    create_engine, Column, Integer, String, ForeignKey, DateTime, Boolean
+    )
 import MeCab
 
-#Classのidについて
-#UserクラスとメッセージクラスにあるIDはdiscordのIDを使用してください。
-#wordクラスにあるIDは単語の順番どおりにIDを振ってください
+# Classのidについて
+# UserクラスとメッセージクラスにあるIDはdiscordのIDを使用してください。
+# wordクラスにあるIDは単語の順番どおりにIDを振ってください
+
 
 class User():
-    def __init__(self,id:int,name:str,bot:bool,mute:bool=False,threat:float=0,messages:list=[]) -> None:
-        self.id = id
-        self.name = name
-        self.bot = bot
-        self.mute = mute
-        self.threat = threat
-        self.messages = messages#messageのIDのリストです。
+    def __init__(
+            self, id: int, name: str, bot: bool,
+            mute: bool = False,
+            threat: float = 0,
+            messages: list = []
+            ) -> None:
+        self.id = id              # ユーザーID
+        self.name = name          # ユーザー名を取得
+        self.bot = bot            # BOTユーザーかどうか
+        self.mute = mute          # ミュートの処理を送信済みか（APIでミュートを確認しているわけではない。）
+        self.threat = threat      # ユーザーの脅威
+        self.messages = messages  # messageのIDのリストです。
 
-    def add_message(self,message_id:int):
+    def add_message(self, message_id: int):
         self.message.append(message_id)
 
     def silence(self):
@@ -28,42 +31,58 @@ class User():
     def unsilence(self):
         self.mute = False
 
+
 class Word():
-    def __init__(self,id:int,word:str,top:int,end:int,):
+    def __init__(self, id: int, word: str, yomi: str, top: int, end: int):
         self.id = id
         self.word = word
+        self.yomi = yomi
         self.top = top
         self.end = end
 
-    def __add__(self,other):
+    def __add__(self, other):
         if type(other) != Word:
             raise TypeError(f"同じクラスではありません。[{other.word}:{type(other)}]")
-        if (self.end +1 - other.top) != 0:
-            raise ValueError(f"単語が連続した順番ではありません。[({self.word,self.top,self.end},{other.word,other.top,other.end})]")
-    
-        return Word(id=None,word=self.word+other.word,top=self.top,end=other.end)
+        if (self.end + 1 - other.top) != 0:
+            raise ValueError(
+                ("単語が連続した順番ではありません。"
+                 f"[({self.word,self.top,self.end}"
+                 f"{other.word,other.top,other.end})]")
+                )
+        return Word(
+            id=None, word=self.word+other.word,
+            top=self.top, end=other.end
+            )
+
+    def __eq__(self, other):
+        if type(other) != Word:
+            raise TypeError(f"同じクラスではありません。[{other.word}:{type(other)}]")
+        return self.word == other.word and self.yomi == self.yomi
+
 
 class Message():
-    def __init__(self,id:int,content:str,user:User,delete:bool,guild:int):
+    def __init__(
+            self, id: int, content: str,
+            user: User, delete: bool,
+            guild: int):
         self.id = id
         self.content = content
-        self.user = user#userのIDです。
+        self.user = user         # userのIDです。
         self.delete = delete
         self.guild = guild
-
 
     def clear(self):
         self.user.messages.pop(self.id)
 
+
 class TmpGuild():
-    def __init__(self,id):
+    def __init__(self, id):
         self.id = id
-        self.messages:list[Message] = []
-        self.users:list[User] = []
+        self.messages: list[Message] = []
+        self.users: list[User] = []
 
-        self.spam_messages:list[Message] = []
-        self.spam_users:list[User] = []
-
+        self.spam_messages: list[Message] = []
+        self.spam_users: list[User] = []
 
 
 def analyze_text(text):
@@ -100,17 +119,16 @@ def analyze_text(text):
 
     return output
 
+
 tmplist = []
 
-async def on_message(message:Message):
-    global tmplist
+
+async def on_message(message: Message, tmplist):
     if message.user.bot:
         return print("処理はパスされました。理由:BOT")
+    # 一時保存場所にギルドデータがない場合は追加する。
+    if message.guild.id not in [guild.id for guild in tmplist]:
+        tmplist.append(TmpGuild(id=message.guild.id))
 
     user = message.user
-    print(message.id,user.name,analyze_text(message.content))
-    
-
-
-
-
+    print(message.id, user.name, analyze_text(message.content))
